80d5bc60556ee8f9470a6aec8c560dd59ae1d851
freimour <leon.rosenfeild@gmail.com>
2021-02-15 09:45:42 -0500

    did not update main before creating branch

--- main_flash.py
+++ main_flash.py
@@ -22,29 +22,30 @@ delay_after_trigger = .3
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(soundPin,GPIO.IN)
+    GPIO.setup(soundPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
     GPIO.setup(flashPin,GPIO.OUT)
     GPIO.setup(shutterPin,GPIO.OUT)
     #wake up flash might be in 'sleep' mode 
     GPIO.output(flashPin,GPIO.HIGH)
     time.sleep(delay_after_flash)
     GPIO.output(flashPin,GPIO.LOW)
-      
-def sound_loop():
     # sleep otherwise the sound from the flash will trigger the sound in the loop()
     time.sleep(.1)
-    while True :
-        if GPIO.input(soundPin) == GPIO.LOW:
-            print ('sound detected....\nset trigger')
-            GPIO.output(shutterPin,GPIO.HIGH)
-            time.sleep(delay_after_trigger)  # delay to ensure shutter is fully open using bulb mode
-            GPIO.output(flashPin,GPIO.HIGH)
-            time.sleep(delay_after_flash)
-            GPIO.output(flashPin,GPIO.LOW)
-            time.sleep(1)  # make sure flash is complete before closing shutter 
-            GPIO.output(shutterPin,GPIO.LOW)
-            freeGPIOchannels()
-            break
+      
+def sound_loop():
+ 
+    GPIO.wait_for_edge(soundPin, GPIO.FALLING)
+    #if GPIO.input(soundPin) == GPIO.LOW:
+    print ('sound detected....\nset trigger')
+    GPIO.output(shutterPin,GPIO.HIGH)
+    time.sleep(delay_after_trigger)  # delay to ensure shutter is fully open using bulb mode
+    GPIO.output(flashPin,GPIO.HIGH)
+    time.sleep(delay_after_flash)
+    GPIO.output(flashPin,GPIO.LOW)
+    time.sleep(1)  # make sure flash is complete before closing shutter 
+    GPIO.output(shutterPin,GPIO.LOW)
+    freeGPIOchannels()
+        
            
 def freeGPIOchannels():
     GPIO.cleanup()
