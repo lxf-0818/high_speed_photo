31697527574dca455409e7784665d189ca177e86
freimour <leon.rosenfeild@gmail.com>
2021-02-19 16:05:22 -0500

    moved trigger shutter back to even loop

--- main_flash.py
+++ main_flash.py
@@ -16,9 +16,10 @@ pin 5
 '''
 soundPin = 19 
 flashPin = 22 
-shutterPin = 18
-switch = 17
+shutterPin = 12
+startSwitch = 17
 photogate = 26
+continueSwitch = 16
 delay_after_flash = 0.05
 delay_after_trigger = .5
 
@@ -31,7 +32,7 @@ def setup():
     GPIO.setmode(GPIO.BCM)
     
     GPIO.setup(soundPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(switch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(startSwitch,GPIO.IN,pull_up_down=GPIO.PUD_UP)
     GPIO.setup(photogate,GPIO.IN,pull_up_down=GPIO.PUD_UP)
     GPIO.setup(flashPin,GPIO.OUT)
     GPIO.setup(shutterPin,GPIO.OUT)
@@ -41,24 +42,29 @@ def setup():
     GPIO.output(flashPin,GPIO.LOW)
    
     time.sleep(.1) #if not sleep will fire delay before openning shutter in bulb mode 
-    GPIO.output(shutterPin,GPIO.HIGH)
-    time.sleep(delay_after_trigger)  # delay to ensure shutter is fully open using bulb mode
+
 
     GPIO.add_event_detect(soundPin, GPIO.FALLING, callback=event_loop, bouncetime=2000) #2000ms 2sec
-    GPIO.add_event_detect(switch, GPIO.FALLING, callback=event_loop, bouncetime=2000)
+    GPIO.add_event_detect(startSwitch, GPIO.FALLING, callback=event_loop, bouncetime=2000)
     GPIO.add_event_detect(photogate, GPIO.FALLING, callback=event_loop, bouncetime=2000)
 
 def event_loop(channel):
     
-    my_dict = {soundPin:'sound detected',switch:'start switch',photogate:'photogate'}
-    
+    my_dict = {soundPin:'sound detected',startSwitch:'start startSwitch',photogate:'photogate'}
     print (my_dict[channel])
+    
+    # just incase flash fired wait before done
+    time.sleep(.1)
+    GPIO.output(shutterPin,GPIO.HIGH)
+    time.sleep(delay_after_trigger)  # delay to ensure shutter is fully open using bulb mode
     
     GPIO.output(flashPin,GPIO.HIGH)
     time.sleep(delay_after_flash)
     GPIO.output(flashPin,GPIO.LOW)
     time.sleep(1)  # make sure flash is complete before closing shutter 
     GPIO.output(shutterPin,GPIO.LOW)
+    GPIO.cleanup()
+    sys.exit(0)
      
 if __name__ == '__main__':    # Program entrance
     print ('Program is starting ... \n')
